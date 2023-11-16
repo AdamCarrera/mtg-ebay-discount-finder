fcac5ca5b3e471b2647d1c71dce0a4d3f88fc96b
Adam Carrera <carrera.adam0@gmail.com>
2023-11-16 03:49:34 -0600

    update scryfall.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main() -> None:
     ebay_handle = ebay.EbayConfig()
     ebay_handle.get_prices()
 
-    scryfall_handle = scryfall.ScryfallConfig(query="")
+    scryfall_handle = scryfall.ScryfallConfig(query="black lotus")
     print(scryfall_handle.search_url)
     scryfall_handle.get_scryfall_prices()
 
--- scryfall.py
+++ scryfall.py
@@ -8,9 +8,16 @@ class ScryfallConfig:
 
     def get_scryfall_prices(self):
         r = requests.get(self.search_url).json()
-        print(r['name'])
+        print("-" * 30)
+        print(f"Name: {r['name']}")
+        print(f"Set: {r['set_name']}")
+        print(f"EDHRec Rank: {r['edhrec_rank']}")
+        print(f"Price: {r['prices']['usd']}")
+        print("-" * 30)
 
     def __post_init__(self):
         if self.search_url is None:
-            self.search_url = f"https://api.scryfall.com/cards/named?fuzzy={self.query}"
+            self.search_url = f"https://api.scryfall.com/cards/all/28"
+
+    def to_collector_number(self, name, set):
         pass
