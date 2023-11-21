775816af05929b546512a7b5e36cf15b081ee6d8
Adam Carrera <carrera.adam0@gmail.com>
2023-11-20 18:13:36 -0600

    refactor scryfall

--- scryfall.py
+++ scryfall.py
@@ -1,10 +1,12 @@
 import requests
 from dataclasses import dataclass
+from typing import Dict
 
 @dataclass
-class ScryfallConfig:
+class Scryfall:
     query: str
     search_url: str = None
+    sets: Dict = None
 
     def get_scryfall_prices(self):
         r = requests.get(self.search_url).json()
@@ -18,12 +20,18 @@ class ScryfallConfig:
         if self.search_url is None:
             self.search_url = f"https://api.scryfall.com/cards/all/28"
 
+        if self.sets is None:
+            r = requests.get("https://api.scryfall.com/sets/").json()['data']
+            self.sets = {item['name']: item['code'] for item in r}
+
+        
+
     def to_collector_number(self, name, set):
         pass
 
-    def get_set_codes(self):
-        r = requests.get("https://api.scryfall.com/sets/").json()
-        for item in r['data']:
-            print(f"Set name: {item['name']}")
-            print(f"Set code: {item['code']}")
-            print("-" * 30)
+    def to_set_code(self, set_name):
+        pass
+        # for item in r['data']:
+        #     print(f"Set name: {item['name']}")
+        #     print(f"Set code: {item['code']}")
+        #     print("-" * 30)
